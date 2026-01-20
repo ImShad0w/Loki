25fd3200cf001bd6579884bae0d8ae0f2502a010
shxd0w <kirillpashnin.dev@gmail.com>
2026-01-20 18:11:38 +0100

    Created short term memory with variables

--- loki/loki_core.py
+++ loki/loki_core.py
@@ -7,6 +7,7 @@ class Loki:
         self.name = "Loki"
         self.time = datetime.now()
         self.user = None
+        self.memory = []
 
     # Input layer
     def hear(self, text):
@@ -25,17 +26,16 @@ class Loki:
             return ("open_app", "terminal")
         if "open" in t and "spotify" in t:
             return ("open_app", "spotify")
-
         if "what time is it" in t:
             return ("get_time", None)
-
         if "who are you" in t:
             return ("identity", None)
-
         if "hello" in t:
             return ("greeting", None)
         if "remember" in t:
-            return ("remember", text)
+            return ("remember", t)
+        if "what do you know" in t:
+            return ("recall", None)
 
         return ("unknown", None)
 
@@ -50,12 +50,17 @@ class Loki:
             return f"I am {self.name}."
         if intent == "greeting":
             return "Hey, let's work on this, shall we?"
+        if intent == "remember":
+            return self.remember(data)
+        if intent == "recall":
+            return self.recall()
 
         return "I didn't understand your request."
 
     # Memory layer
-    # def remember(self, data):
-    # TODO: Add filtering on what to remember
+    def remember(self, data):
+        self.memory.append(data)
+        print("I will remeber that.")
 
     # Action layer
     def open_app(self, app):
@@ -72,3 +77,6 @@ class Loki:
 
     def tell_time(self):
         return f"The time is: {self.time.hour}:{self.time.minute:02d}"
+
+    def recall(self):
+        return f"You said: {self.memory}"
